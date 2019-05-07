e3919c79b9666ace0ca2f2ff2681d59d4faba9f2
Mr. MR Ingram <u15194257@up.ac.za>
2019-05-07 14:20:04 +0200

    new commit

--- site.js
+++ site.js
@@ -7,3 +7,7 @@
 function abeFunction() {
 	alert("Excited for INF 354 because it's useful code");
 }
+
+function MarZFunction() {
+	alert("Excited for INF 354 because it's one step closet to finishing my degree");
+}
